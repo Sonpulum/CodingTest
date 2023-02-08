2dca11ee3b7e0c2918affa4f1f918058eee02ac9
Sonpulum <47966291+Sonpulum@users.noreply.github.com>
2023-02-08 09:27:52 +0900

    [Silver IV] Title: 덱, Time: 80 ms, Memory: 34308 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().strip())
+
+queue = deque()
+for _ in range(n):
+    cmd = list(map(str, sys.stdin.readline().split()))
+
+    if cmd[0] == 'push_front':
+        queue.appendleft(cmd[1])
+
+    elif cmd[0] == 'push_back':
+        queue.append(cmd[1])
+
+    elif cmd[0] == 'pop_front':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    
+    elif cmd[0] == 'pop_back':
+        if queue:
+            print(queue.pop())
+        else:
+            print(-1)
+
+    elif cmd[0] == 'size':
+        print(len(queue))
+
+    elif cmd[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    
+    elif cmd[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+
+    elif cmd[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
