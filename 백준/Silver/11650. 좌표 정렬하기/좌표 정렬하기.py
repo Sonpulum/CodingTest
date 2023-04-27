f914559f30c2ba3a14ba625332a52dc49b65bcae
Sonpulum <47966291+Sonpulum@users.noreply.github.com>
2023-04-27 19:45:32 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 376 ms, Memory: 59204 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,8 @@
+import sys
+n = int(sys.stdin.readline().strip())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+arr.sort(key = lambda x : (x[0],x[1]))
+
+for i in arr:
+  print(i[0],i[1])
