bba22e5f92c73091559cdf7754b08d4bcc3b5c51
손푸름 <47966291+Sonpulum@users.noreply.github.com>
2025-03-18 22:44:36 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,27 @@
+import sys
 from collections import deque
-n,m = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(n)]
 
-chk = [[False]*m for _ in range(n)]
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
-    chk[x][y] = True
-    while queue:
-        ex,ey = queue.popleft()
+def bfs(st_x, st_y):
+    visit_graph = [[False for _ in range(m)] for _ in range(n)]
 
-        for k in range(4):
-            nx = ex+dx[k]
-            ny = ey+dy[k]
+    que = deque()
+    que.append((st_x,st_y))
 
-            if 0<=nx<m and 0<=ny<n:
-                if chk[ny][nx] == False and graph[ny][nx] == 1:
-                    chk[ny][nx] = True
-                    graph[ny][nx] += graph[ey][ex]
-                    queue.append((nx,ny))
-
-
+    while que:
+        cur_x, cur_y = que.popleft()
+        visit_graph[cur_y][cur_x] = True
+        for i in range(4):
+            next_x = cur_x + dx[i]
+            next_y = cur_y + dy[i]
+            if 0 <= next_x < m and 0 <= next_y < n:
+                if graph[next_y][next_x] != 0 and visit_graph[next_y][next_x] == False:
+                    graph[next_y][next_x] = graph[cur_y][cur_x] + 1
+                    que.append((next_x, next_y))
+                    visit_graph[next_y][next_x] = True
 bfs(0,0)
 print(graph[n-1][m-1])
